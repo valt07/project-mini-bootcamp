f41367dee9751d75f89c076d464243284c577466
dearyycl <airinaryanty69@gmail.com>
2025-12-25 14:59:40 +0800

    model2

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -14,7 +14,7 @@ class Article extends Model {
     ];
 
     public function author() {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'user_id');
     }
     
     public function category() {
