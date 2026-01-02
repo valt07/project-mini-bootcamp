dbba67b010d4b60a83eebf37c76e2a7ebf8aa1f8
unknown <akbardirham15@gmail.com>
2026-01-02 19:08:51 +0800

    Pages Frontend

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,3 +2,4 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.withCredentials = true; // Important for Sanctum cookie auth if used, or just good practice.
