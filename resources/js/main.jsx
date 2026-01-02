0b5d09df88b4e041ba45dd5895dbd0ed29258e42
Agung <agung.subekthi07@gmail.com>
2026-01-02 14:37:12 +0800

    penggabungan frontend dan backend

--- resources/js/main.jsx
+++ resources/js/main.jsx
@@ -1,3 +1,4 @@
+import './bootstrap';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -5,6 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    {/* <div>Hello World</div> */}
     <App />
   </StrictMode>,
 )
