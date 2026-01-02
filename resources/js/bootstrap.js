264212b405fbf74f8b06c8fefd6c3cf45a36a980
valt07 <agung.subekthi07@gmail.com>
2026-01-02 18:09:24 +0800

    Revert "Team/akbar"

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,4 +2,3 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-window.axios.defaults.withCredentials = true; // Important for Sanctum cookie auth if used, or just good practice.
